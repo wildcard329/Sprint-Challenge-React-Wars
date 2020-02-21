0e25007fe96e618ed0698cfc9c3eee853fe308a0
wildcard329 <enkiduwildman@gmail.com>
2020-02-21 08:15:33 -0800

    initialized challenge, installed dependencies, created StarWars and CardMaker components

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import StarWars from './components/StarWars';
 import './App.css';
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <StarWars />
     </div>
   );
 }
